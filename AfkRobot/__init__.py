648768a1ed176ce77262361b9a5ae84f89ccf695
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-07-03 22:27:33 +0530

    Update __init__.py

--- AfkRobot/__init__.py
+++ AfkRobot/__init__.py
@@ -24,7 +24,7 @@ cleanmode = {}
 SUDOERS = config.SUDO_USER
 
 app = Client(
-    "AfkBotV2",  # new name = new session
+    "FreshBotSession",  # <== new session name
     config.API_ID,
     config.API_HASH,
     bot_token=config.BOT_TOKEN,
