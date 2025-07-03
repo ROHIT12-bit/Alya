c2f446b852e5102931838359ceaf718c8d37b644
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-07-03 23:20:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ API_HASH = getenv("API_HASH", "72095ec36984aa9ceb0dbaa9cec31559")
 BOT_TOKEN = getenv("BOT_TOKEN", "7781735913:AAFa3jydrpzBCyULjVZ6hoYdsdkwDOthd_o")
 LOG_ID = int(getenv("LOG_ID", "-1002640844591"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("MONGO_DB_URI", ")mongodb+srv://rohitreddyathuru:R6Co7MOjTYQOAqcq@cluster0.xrwjpl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 SUDO_USER = list(map(int, getenv("SUDO_USER", "20366634").split()))
